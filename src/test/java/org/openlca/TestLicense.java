405614f3cb743ea27a5ebd5471fdf99a6f1bfdf0
François Le Rall <lerall.francois@gmail.com>
2023-05-12 15:09:30 +0200

    Create a license generator.  - the license generator consists in:   - getting the key pair from the CA,   - create a CSR and sign it with the CA key pair,   - add extension to the issued certificate.  - the license cannot be parametrized yet.

---
+++ src/test/java/org/openlca/TestLicense.java
@@ -0,0 +1,13 @@
+package org.openlca;
+
+import org.junit.Test;
+import org.openlca.license.License;
+
+public class TestLicense {
+
+	@Test
+	public void testCreateCA() {
+		var license = new License();
+	}
+
+}
